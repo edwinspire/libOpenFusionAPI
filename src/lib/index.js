a8b46d7eda89f036615d4e25598bad702ad46af3
Edwin De La Cruz <edwinspire@gmail.com>
2024-01-12 22:56:53 -0500

    Se carga app en la CACHE

--- src/lib/index.js
+++ src/lib/index.js
@@ -6,7 +6,33 @@ import websocket from "@fastify/websocket";
 import fastifyStatic from "@fastify/static";
 import { fileURLToPath } from "url";
 import { dirname, join } from "path";
+
+import dbAPIs from "./db/sequelize.js";
 import { defaultApps, getAppByName, getAppWithEndpoints } from "./db/app.js";
+import { defaultEndpoints } from "./db/endpoint.js";
+import { defaultUser, login } from "./db/user.js";
+import { getRoleById } from "./db/role.js";
+import { createPathRequest } from "./db/path_request.js";
+import { defaultMethods } from "./db/method.js";
+import { defaultHandlers } from "./db/handler.js";
+import { prefixTableName } from "./db/models.js";
+import { runHandler } from "./handler/handler.js";
+import { createFunction } from "./handler/jsFunction.js";
+import { getApiHandler } from "./db/app.js";
+
+import {
+  key_endpoint_method,
+  struct_path,
+  get_url_params,
+  path_params,
+  mqtt_path_params,
+  path_params_to_url,
+  key_url_from_params,
+  internal_url_hooks,
+  websocket_hooks_resource,
+  getPartUrl,
+  //	defaultSystemPath
+} from "./server/utils_path.js";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -35,6 +61,12 @@ export default class ServerAPI extends EventEmitter {
   constructor({ buildDB = false } = {}) {
     super();
 
+    if (!process.env.PORT) {
+      throw { error: "PORT is required" };
+    }
+
+    this._cacheEndpoint = new Map();
+
     this.fastify = Fastify({
       logger: true,
     });
@@ -51,22 +83,34 @@ export default class ServerAPI extends EventEmitter {
 
     this.buildDB();
 
-    this.fastify.addHook("preValidation", (request, reply, done) => {
-      console.log(">>>>>>> preValidation");
+    this.fastify.addHook("preValidation", async (request, reply) => {
+      let request_path_params = get_url_params(request.url);
 
-      if (!request.url.startsWith('/api')) {
-					console.log(' ::: req.path >>>>', request.url);
-				
-			}
+      console.log(">>>>>>> preValidation", request_path_params);
+
+      if (!request.url.startsWith("/api")) {
+        console.log(" ::: req.path >>>>", request.url);
+      }
+
+      if (request_path_params && request_path_params.path) {
+
+        let path_endpoint_method = key_endpoint_method(
+          request_path_params.app,
+          request_path_params.resource,
+          request_path_params.environment,
+          request.method
+        );
 
 
-      // Cargar la aplicación
-
-      //
-
-      // some code
-      done();
-    });
+        //
+        if (!this._cacheEndpoint.has(path_endpoint_method)) {
+          // No está en cache, se obtiene todos los endpoints de la aplicación y la carga en CACHE
+          this._loadEndpointsByAPPToCache(request_path_params.app);
+        }
+      } else {
+        reply.code(404).send({ error: "Not Found" });
+      }
+});
 
     this.fastify.get("/ws/*", { websocket: true }, (connection, req) => {
       console.log("sssssss");
@@ -78,13 +122,136 @@ export default class ServerAPI extends EventEmitter {
     });
 
     // Declare a route
-    this.fastify.all("/api/:animal/:cosa", (request, reply) => {
+    this.fastify.all(struct_path, (request, reply) => {
       reply.send({ hello: "world" });
     });
 
     const port = process.env.PORT || 3000;
     console.log("Listen on PORT " + port);
     await this.fastify.listen({ port: port });
+  }
+
+  _getApiHandler(app_name, endpointData, appVarsEnv) {
+    let returnHandler = {};
+    returnHandler.params = endpointData;
+
+    try {
+      appVarsEnv =
+        typeof appVarsEnv !== "object"
+          ? JSON.parse(appVarsEnv)
+          : appVarsEnv ?? {};
+
+      let appVars = appVarsEnv[endpointData.environment];
+
+      if (endpointData.enabled) {
+        // @ts-ignore
+        if (returnHandler.params.is_public) {
+          returnHandler.authentication = async () => {
+            //console.log('authentication, public: ', jw_token);
+            return true;
+          };
+        } else {
+          // @ts-ignore
+          returnHandler.authentication = async (
+            /** @type {string} */ jw_token
+          ) => {
+            return checkAPIToken(app_name, endpointData, jw_token);
+          };
+        }
+
+        // @ts-ignore
+        returnHandler.params.code = returnHandler.params.code || "";
+
+        if (appVars && typeof appVars === "object") {
+          const props = Object.keys(appVars);
+
+          for (let i = 0; i < props.length; i++) {
+            const prop = props[i];
+
+            //	console.log('typeof appData.vars[prop]: ', appVars[prop], typeof appVars[prop]);
+
+            switch (typeof appVars[prop]) {
+              case "string":
+                // @ts-ignore
+                returnHandler.params.code = returnHandler.params.code.replace(
+                  prop,
+                  appVars[prop]
+                );
+                break;
+              case "object":
+                // @ts-ignore
+                returnHandler.params.code = returnHandler.params.code.replace(
+                  '"' + prop + '"',
+                  JSON.stringify(appVars[prop])
+                );
+
+                // @ts-ignore
+                returnHandler.params.code = returnHandler.params.code.replace(
+                  prop,
+                  JSON.stringify(appVars[prop])
+                );
+                break;
+            }
+          }
+        }
+
+        // @ts-ignore
+        if (returnHandler.params.handler == "JS") {
+          // @ts-ignore
+          returnHandler.params.jsFn = createFunction(
+            returnHandler.params.code,
+            appVars
+          );
+        }
+        returnHandler.message = "";
+        returnHandler.status = 200;
+      } else {
+        returnHandler.message = `Method ${endpointData.method} Unabled`;
+        returnHandler.status = 404;
+        //console.log(endpointData);
+      }
+    } catch (error) {
+      // @ts-ignore
+      returnHandler.message = error.message;
+      returnHandler.status = 505;
+      console.trace(error);
+    }
+
+    return returnHandler;
+  }
+
+  /**
+   * @param {any} app
+   */
+  async _loadEndpointsByAPPToCache(app) {
+    try {
+      // Carga los endpoints de una App a cache
+      let appDataResult = await getAppWithEndpoints({ app: app }, false);
+
+      if (appDataResult && appDataResult.length > 0) {
+        const appDatas = appDataResult.map((result) => result.toJSON());
+
+        const appData = appDatas[0];
+
+        for (let i = 0; i < appData.apiserver_endpoints.length; i++) {
+          let endpoint = appData.apiserver_endpoints[i];
+
+          let url_app_endpoint = key_endpoint_method(
+            appData.app,
+            endpoint.resource,
+            endpoint.environment,
+            endpoint.method
+          );
+
+          this._cacheEndpoint.set(
+            url_app_endpoint,
+            this._getApiHandler(appData.app, endpoint, appData.vars)
+          );
+        }
+      }
+    } catch (error) {
+      console.trace(error);
+    }
   }
 
   /**
@@ -98,7 +265,7 @@ export default class ServerAPI extends EventEmitter {
 
       (async () => {
         try {
-          await dbRestAPI.sync({ alter: true });
+          await dbAPIs.sync({ alter: true });
           await defaultUser();
           await defaultMethods();
           await defaultHandlers();
--- src/lib/server/utils_path.js
+++ src/lib/server/utils_path.js
@@ -1,24 +1,49 @@
-import { match } from 'path-to-regexp';
+import { match } from "path-to-regexp";
 
 //export const struct_path = '/api/:app/:namespace/:name/:version/:environment';
-export const struct_path = '/api/:app/:environment*';
+export const struct_path = "/api/:app/*";
 
 export const internal_url_hooks = "/websocket/internat/prd/hooks";
 export const websocket_hooks_resource = "/websocket/hooks";
 
-const fn_match_url = match(struct_path, { decode: decodeURIComponent });
+const fn_match_url = match("/api/:app*", { decode: decodeURIComponent });
 
-const mqtt_struct_path = '/api/:app/:namespace/:name/:version/:environment/:username/:topic*';
-const fn_mqtt_match_url = match(mqtt_struct_path, { decode: decodeURIComponent });
+const mqtt_struct_path =
+  "/api/:app/:namespace/:name/:version/:environment/:username/:topic*";
+const fn_mqtt_match_url = match(mqtt_struct_path, {
+  decode: decodeURIComponent,
+});
 
 export const path_params = (/** @type {string} */ url) => {
-	let reqUrl = new URL(`http://localhost${url}`);
-	return fn_match_url(reqUrl.pathname);
+  let reqUrl = new URL(`http://localhost${url}`);
+  return fn_match_url(reqUrl.pathname);
+};
+
+export const get_url_params = (/** @type {string} */ url) => {
+  let reqUrl = new URL(`http://localhost${url}`);
+  let fn = match("/api/:parts*", { decode: decodeURIComponent });
+  let par = fn(reqUrl.pathname);
+
+  if (par.params) {
+    par.app =
+      par.params.parts && par.params.parts.length > 0
+        ? par.params.parts[0]
+        : undefined;
+    par.environment =
+      par.params.parts && par.params.parts.length > 1
+        ? par.params.parts.slice(-1)[0]
+        : undefined;
+        par.resource = '/'+par.params.parts.slice(1, -1).join('/');
+  } else {
+    par = { params: { parts: [] } };
+  }
+
+  return par;
 };
 
 export const mqtt_path_params = (/** @type {string} */ url) => {
-	let reqUrl = new URL(`http://localhost${url}`);
-	return fn_mqtt_match_url(reqUrl.pathname);
+  let reqUrl = new URL(`http://localhost${url}`);
+  return fn_mqtt_match_url(reqUrl.pathname);
 };
 
 //export const path_params_to_url = (/** @type {{ app: any; namespace: any; name: any; version: any; environment: any; }} */ params) => {
@@ -26,51 +51,55 @@ export const mqtt_path_params = (/** @type {string} */ url) => {
 //}
 
 export const path_params_to_url = (
-	/** @type {{ app: string; environment: string; resource: string; }} */ params
+  /** @type {{ app: string; environment: string; resource: string; }} */ params
 ) => {
-	return `/api/${params.app}/${params.environment}${params.resource}`;
+  return `/api/${params.app}/${params.environment}${params.resource}`;
 };
 
 export const key_url_from_params = (
-	/** @type {{ app: string; method: string; resource: string; }} */ params
+  /** @type {{ app: string; method: string; resource: string; }} */ params
 ) => {
-	return `/api/${params.app}${params.resource}/${params.method}`;
+  return `/api/${params.app}${params.resource}/${params.method}`;
+};
+
+export const key_endpoint_method = (app, resource, environment, method) => {
+  return `/api/${app}${resource}/${environment}|${method}`;
 };
 
 /**
  * @param {string} string_url
  */
 export function validateURL(string_url) {
-	const patron = /^\/[a-zA-Z0-9\-._~%]+(\/[a-zA-Z0-9\-._~%]+)*\/?$/;
-	// ^ Inicio de la cadena
-	// \/ Barra inicial
-	// [a-zA-Z0-9\-._~%]+ Uno o más caracteres permitidos en la ruta
-	// (\/[a-zA-Z0-9\-._~%]+)* Cero o más grupos adicionales de barra seguido de caracteres permitidos
-	// \/? Barra opcional al final
-	// $ Fin de la cadena
+  const patron = /^\/[a-zA-Z0-9\-._~%]+(\/[a-zA-Z0-9\-._~%]+)*\/?$/;
+  // ^ Inicio de la cadena
+  // \/ Barra inicial
+  // [a-zA-Z0-9\-._~%]+ Uno o más caracteres permitidos en la ruta
+  // (\/[a-zA-Z0-9\-._~%]+)* Cero o más grupos adicionales de barra seguido de caracteres permitidos
+  // \/? Barra opcional al final
+  // $ Fin de la cadena
 
-	if (patron.test(string_url)) {
-		return true;
-	} else {
-		return false;
-	}
+  if (patron.test(string_url)) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 /**
  * @param {string} url
  */
 export function getPartUrl(url) {
-	const partes = url.split('/').filter(part => part !== ''); // Elimina elementos vacíos
-  
-	if (partes.length >= 4 && partes[0] === 'api') {
-	  return {
-		url: url,
-//		api: partes[0],
-		app: partes[1],
-		env: partes[2],
-		resource: '/' + partes.slice(3).join('/')
-	  };
-	} else {
-	  return { error: 'URL no válida' };
-	}
+  const partes = url.split("/").filter((part) => part !== ""); // Elimina elementos vacíos
+
+  if (partes.length >= 4 && partes[0] === "api") {
+    return {
+      url: url,
+      //		api: partes[0],
+      app: partes[1],
+      env: partes[2],
+      resource: "/" + partes.slice(3).join("/"),
+    };
+  } else {
+    return { error: "URL no válida" };
   }
+}
