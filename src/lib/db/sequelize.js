b62e3f3755bb901f731db19af627e06be3ea03c1
EDWIN ROBERTO DE LA CRUZ ATAHUALPA <edwindelacruz@Farmaenlace.com>
2025-12-03 22:10:06 -0500

    Se agrega la variable de entorno USE_HEROKU_POSTGRESQL que debe ser seteada  TRUE para que tome las opciones correctas para poder conectar a la base de datos Postgresql en Heroku

--- src/lib/db/sequelize.js
+++ src/lib/db/sequelize.js
@@ -5,17 +5,28 @@ import { Sequelize } from "sequelize";
 //Temporal DDBB
 const tmpPath = path.join(os.tmpdir(), "ofapi.sqlite");
 
-const db_conn = process.env.DATABASE_URL || process.env.DATABASE_URI_API || `sqlite:${tmpPath}`;
+const db_conn =
+  process.env.DATABASE_URL ||
+  process.env.DATABASE_URI_API ||
+  `sqlite:${tmpPath}`;
 
-const dbsequelize = new Sequelize(db_conn, {
+let dialectOptions;
+if (process.env.USE_HEROKU_POSTGRESQL) {
+  dialectOptions = { ssl: { rejectUnauthorized: false } };
+}
+
+const options = {
   logging: false, // Imprime el SQL en consola
+  dialectOptions: dialectOptions,
   pool: {
-    max: db_conn.includes("sqlite") ? 1 : 10,
-    min: 0,
+    max: db_conn.includes("sqlite") ? 1 : 20,
+    min: 1,
     acquire: 30000,
     idle: 10000,
   },
-});
+};
+
+const dbsequelize = new Sequelize(db_conn, options);
 
 export default dbsequelize;
 
@@ -23,11 +34,13 @@ export default dbsequelize;
   try {
     await dbsequelize.authenticate();
     console.log(
-      ">>>>>>>>> Connection has been established successfully to " + db_conn
+      ">>>>>>>>> Connection has been established successfully to " + db_conn,
+      options
     );
   } catch (error) {
     console.error(
       ">>>>>>>>> Unable to connect to the database: " + db_conn,
+      options,
       error
     );
   }
