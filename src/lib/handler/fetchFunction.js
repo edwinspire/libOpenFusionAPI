cafa85098a0443911ef5bac023ea3fd78e353b01
EDWIN ROBERTO DE LA CRUZ ATAHUALPA <edwindelacruz@Farmaenlace.com>
2025-12-09 16:40:50 -0500

    fetchFunction.js revisado por IA

--- src/lib/handler/fetchFunction.js
+++ src/lib/handler/fetchFunction.js
@@ -1,46 +1,69 @@
 // @ts-ignore
-import uFetch from '@edwinspire/universal-fetch';
-import {setCacheReply} from "./utils.js";
+import uFetch from "@edwinspire/universal-fetch";
+import { setCacheReply } from "./utils.js";
 export const fetchFunction = async (
-	/** @type {{
+  /** @type {{
 	  url(url, init): unknown method?: any; headers: any; body: any; query: any; 
 }} */ $_REQUEST_,
-	/** @type {{ status: (arg0: number) => { (): any; new (): any; json: { (arg0: { error: any; }): void; new (): any; }; }; }} */ response,
-	/** @type {{ handler?: string; code: any; }} */ method
+  /** @type {{ status: (arg0: number) => { (): any; new (): any; json: { (arg0: { error: any; }): void; new (): any; }; }; }} */ response,
+  /** @type {{ handler?: string; code: any; }} */ method
 ) => {
-	//console.log(uFetch);
-	try {
-		let req_headers = { ...$_REQUEST_.headers };
-		delete req_headers['content-length'];
-		delete req_headers['host'];
-		delete req_headers['connection'];
+  //console.log(uFetch);
+  try {
+    let req_headers = { ...$_REQUEST_.headers };
+    delete req_headers["content-length"];
+    delete req_headers["host"];
+    delete req_headers["connection"];
 
-		let init = {
-			headers: req_headers, // Usar los headers de la peticion
-			body: $_REQUEST_.body, // Usar los body de la peticion
-			query: $_REQUEST_.query, // Usar los query de la peticion,
-			url: method.code
-		};
+    /** ------------------------------
+     *  SANITIZAR HEADERS
+     * ------------------------------*/
+    const forwardedHeaders = { ...$_REQUEST_.headers };
 
-		// console.log($_REQUEST_);
+    delete forwardedHeaders["content-length"];
+    delete forwardedHeaders["host"];
+    delete forwardedHeaders["connection"];
+    delete forwardedHeaders["x-forwarded-for"];
 
-		const FData = new uFetch();
-		// @ts-ignore
-		let resp = await FData[$_REQUEST_.method.toUpperCase()](init);
+    /** ------------------------------
+     *  VALIDAR URL DESTINO
+     * ------------------------------*/
+    if (
+      !method.code ||
+      typeof method.code !== "string" ||
+      method.code.length == 0
+    ) {
+      response
+        .code(500)
+        .send({ error: `The destination URL ${method.code} is invalid.` });
+    }
 
-		let r = await resp.json();
-		
-		// @ts-ignore
-		if (response.openfusionapi.lastResponse && response.openfusionapi.lastResponse.hash_request) {
-			// @ts-ignore
-			response.openfusionapi.lastResponse.data = r;
-		}
-		
-		
-		response.code(resp.status).send(r);
-	} catch (error) {
-	 setCacheReply(response, error);
-		// @ts-ignore
-		response.code(500).send(error);
-	}
+    let init = {
+      headers: forwardedHeaders, // Usar los headers de la peticion
+      body: $_REQUEST_.body, // Usar los body de la peticion
+      query: $_REQUEST_.query, // Usar los query de la peticion,
+      url: method.code,
+    };
+
+    const FData = new uFetch();
+    // @ts-ignore
+    let resp = await FData[$_REQUEST_.method.toUpperCase()](init);
+
+    let r = await resp.json();
+
+    // @ts-ignore
+    if (
+      response.openfusionapi.lastResponse &&
+      response.openfusionapi.lastResponse.hash_request
+    ) {
+      // @ts-ignore
+      response.openfusionapi.lastResponse.data = r;
+    }
+
+    response.code(resp.status).send(r);
+  } catch (error) {
+    setCacheReply(response, error);
+    // @ts-ignore
+    response.code(500).send(error);
+  }
 };
